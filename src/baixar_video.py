eafb3512aad8f8b41f8f3e589c5665df5c851406
LuizEduardo-service <luizeduardo.service@outlook.com>
2023-02-08 16:13:47 -0300

    Iniciando Projeto

---
+++ src/baixar_video.py
@@ -0,0 +1,11 @@
+from pytube import YouTube
+
+class GeradorDeVideoYT():
+    def __init__(self, link: str) -> None:
+        self.link = link
+
+    def valida_link(self):
+        pass
+
+    def baixar_video(self):
+        pass
---
+++ view/tela.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+import tkinter
+
+class TelaPrincial():
+
+    def __init__(self) -> None:
+        pass
